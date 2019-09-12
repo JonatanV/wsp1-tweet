ab2921c19e0c80acb806744dba1eb9d4d3150bb3
JonatanV <nofettatjarn1>
2019-09-12 10:53:48 +0200

    php mera

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">   
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href = "css/main.css" rel="stylesheet">
-    <script src = "js/main.js"></script>
-    <title>Document</title>
-</head>
-<body>
-    <div class="container"
-        ><h1>Den fantastiska tweetsidan</h1>
+
 <?php
 include 'include/dbinfo.php';
-echo $username;
+
 
 include 'include/dbinfo.php';
 // mysql -u -p
@@ -35,16 +23,7 @@ $sth = $dbh->prepare('SELECT * FROM tweet
 $sth->execute();
 $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 //echo "<pre>" . print_r($result,1) . "</pre>";
-foreach ($result as $row) {
-    //echo "<pre>" . print_r($row,1) . "</pre>";
-    echo "<div>";
-    echo "<h2>" . $row['body'] . "</h2>";
-    echo "<p>" . $row['name'] . "</p>";
-    echo "</div>";
-}
-        ?>
-        <input type="button" onclick="myFunction()">
-        
-    </div>
-</body>
-</html>
+
+include 'views/index_layout.php';
+?>
+ 
